28e413cb57d6083dd990c458dd84af42e83df392
Michael Youngblood <682868+aliendev@users.noreply.github.com>
2026-09-24 15:03:46 -0500

    the setup is ready

--- main.go
+++ main.go
@@ -1,7 +1,31 @@
 package main
 
-import "fmt"
+import (
+    "flag"
+    "fmt"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+
+    // define flags
+    helpFlag := flag.Bool("help", false, "Show help message")
+    versionFlag := flag.Bool("version", false, "Show version message")
+    addTripFlag := flag.Bool("add-trip", false, "Add a trip")
+    addExpenseFlag := flag.Bool("add-expense", false, "Add an expense")
+    viewTripsFlag := flag.Bool("view-trips", false, "View trips")
+    viewExpensesFlag := flag.Bool("view-expenses", false, "View expenses")
+    viewReportsFlag := flag.Bool("view-reports", false, "View reports")
+
+
+    // define initial help message
+    oopsMessage := "Usage: ./favor [command]"
+
+    flag.Parse()
+    args := flag.Args()
+
+    if len(args) == 0 {
+        fmt.Println(oopsMessage)
+    }
+
+
 }
